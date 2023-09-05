5a116b810c46b2c88699650ef08e8ddce24aa83a
Shohrux <shoxruxsharipov1105@gmail.com>
2023-09-05 06:17:19 +0500

    1st commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity2.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        val binding = AppCompatActivity
+            setContentView(R.layout.activity_main)
+
+    }
+}
